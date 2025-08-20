665d522fa9f01dbbf5c51ae390e800ef86bbf221
hoggspjot <Andreyid67@gmail.com>
2025-08-20 22:55:28 +0300

    skeleton

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
+        Parrot p = new Parrot("Parrot");
+
+    }
+}
---
+++ src/main/java/Parrot.java
@@ -0,0 +1,15 @@
+public class Parrot {
+    private String name;
+
+    public Parrot(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
