e768d0c5ce1c43a6fae0559a2d19c2d8c355d1dc
Hoggspjot <vingisaar@gmail.com>
2025-08-28 22:55:01 +0300

    BeanDefinition

--- src/main/java/com/example/springstudybook/Main.java
+++ src/main/java/com/example/springstudybook/Main.java
@@ -14,7 +14,10 @@ public class Main {
         x.setName("Kiki");
 
         Supplier<Parrot> parrotSupplier = () -> x;
-        context.registerBean("parrot1", Parrot.class, parrotSupplier);
+        context.registerBean("parrot1",
+                Parrot.class,
+                parrotSupplier,
+                bc -> bc.setPrimary(true));
 
         Parrot p = context.getBean(Parrot.class);
         System.out.println(p.getName());
