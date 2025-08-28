9794bcf82c8da70b0345ae0158ef338078075926
Hoggspjot <vingisaar@gmail.com>
2025-08-28 23:12:29 +0300

    2 лекция первая настройка

---
+++ src/main/java/Config.java
@@ -0,0 +1,21 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public Parrot parrot() {
+        Parrot p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+    @Bean
+    public Person person() {
+        Person p = new Person();
+        p.setName("Ella");
+        return p;
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(Config.class);
+
+        Person person = context.getBean(Person.class);
+
+        Parrot parrot = context.getBean(Parrot.class);
+
+        System.out.println(person.getName());
+        System.out.println(parrot.getName());
+        //Убеждаемся что связь не установлена
+        System.out.println(person.getParrot());
+    }
+}
