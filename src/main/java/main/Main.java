3d7bd3220d431c5945dc3a55ffef6951d8e8620c
Hoggspjot <vingisaar@gmail.com>
2025-08-29 23:34:07 +0300

    3 лекция 2 версия c spring

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,18 +1,20 @@
 package main;
 
 import model.Comment;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import proxies.EmailCommentNotificationProxy;
 import repositories.DBCommentRepository;
 import services.CommentService;
 
 public class Main {
     public static void main(String[] args) {
-        var commentRepository = new DBCommentRepository();
-        var commentNotificationProxy = new EmailCommentNotificationProxy();
-        var commentService = new CommentService(commentRepository, commentNotificationProxy);
-        var comment = new Comment();
-        comment.setAuthor("Lua");
-        comment.setText("Demo comment");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(Config.class);
+        Comment comment = new Comment();
+        comment.setAuthor("Leo");
+        comment.setText("Hello World");
+
+        CommentService commentService = context.getBean(CommentService.class);
         commentService.publishComment(comment);
     }
 }
