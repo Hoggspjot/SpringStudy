cb0de8b6327433a3b2c49bb4a6b58d5b4c4177c0
Hoggspjot <vingisaar@gmail.com>
2025-08-30 23:21:42 +0300

    4 лекция Singleton

---
+++ src/main/java/Config.java
@@ -0,0 +1,15 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import services.CommentService;
+
+@Configuration
+public class Config {
+
+
+    @Bean
+    public CommentService commentService() {
+        return new CommentService();
+    }
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(Config.class);
+
+        CommentService cs1 = context.getBean("commentService", CommentService.class);
+        CommentService cs2 = context.getBean("commentService", CommentService.class);
+
+        boolean b1 = cs1 == cs2;
+        System.out.println(b1);
+
+    }
+}
