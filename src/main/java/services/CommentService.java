d0094a48427a0ad3cb0622b4ea83fde5118fcd72
Hoggspjot <vingisaar@gmail.com>
2025-08-31 22:31:32 +0300

    4 лекция Lazy создание экземпляра бина

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -2,13 +2,16 @@ package services;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import repositories.CommentRepository;
 
 @Service
+@Lazy
 public class CommentService {
 
     private final CommentRepository commentRepository;
+
 
     @Autowired
     public CommentService(CommentRepository commentRepository) {
