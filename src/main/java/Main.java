4250da034bfc906a9a61cac809f4355e08ef1324
Hoggspjot <vingisaar@gmail.com>
2025-09-01 23:33:02 +0300

    5 лекция костяк без аспектов

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContextExtensionsKt;
+import services.Comment;
+import services.CommentService;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,7 +9,12 @@ public class Main {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(Config.class);
 
+        CommentService cs = context.getBean(CommentService.class);
+        Comment comment = new Comment();
+        comment.setText("Demo comment");
+        comment.setAuthor("Vasya");
 
+        cs.publishComment(comment);
 
     }
 }
---
+++ src/main/java/services/CommentService.java
@@ -0,0 +1,17 @@
+package services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class CommentService {
+
+    Logger logger = Logger.getLogger(CommentService.class.getName());
+
+
+    public void publishComment(Comment comment) {
+        logger.info("Publishing comment" + comment.getText());
+    }
+
+}
